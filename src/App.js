4a6e972fb72f8900b09a02b739a988bf9fa39cd2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-12 16:51:15 -0800

    create very basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How Fast Can You Type?
+      </h1>
+      <textarea style={{width: '50%', height: '20em'}}/>
+      <h4>Seconds Remaining:</h4>
+      <button>Start!</button>
+      <h1>Word Count:</h1>
     </div>
   );
 }
